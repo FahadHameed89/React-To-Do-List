177d0fa8d03525861bb4dc913f58d19af08f8508
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-09-11 11:40:12 -0600

    Add onchange event; add newTask state; useState() example

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 // Create a component function
 function ToDo ()
-{
+{   // Assignment deconstructor array. It places returns info to multiple data sources, like filling an array
+
+/*     const [num1, num2] = [1, 2];
+    console.log ( num1);
+    console.log (num2);  */
+
+    /*Set up a state - '' is default. 
+        First item is a varaiable that holds the state value
+        Second value is a function that we use to update the state value
+        Important! We will never update the first item Directly...EVER!
+
+    */
+    const [newTask, setNewTask] = useState( 'default' ); // Argument in useState()' is the default value for this state. 
+
     return (    // The return is the output of the component, we will use a form, which will render
         <>
         <form>
             <label htmlFor="task">New Task:</label>
-            <input type="text" id="task" ></input>
+            <input 
+            type="text" 
+            id="task" 
+            onChange= { e => { setNewTask ( e.target.value ) } }
+            value= { newTask } />
+            <p>
+                <strong>Current Task Value:</strong>
+                <em>{newTask}</em>
+            </p>
             <input type="submit" value="Add To-Do" />
         </form>
         <ul></ul>
